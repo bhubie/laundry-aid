a4e601ef957443c2c340b2e0e365e1156d14484d
Brett Huber <bretthuber6@gmail.com>
2017-07-26 22:53:16 -0500

    added start of config file for storing washing cylcles and times

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
